3fd8435c81c84a1124efad4775ee0eec97cf42a0
Weslley <46580025+WeslleyBino@users.noreply.github.com>
2021-04-15 17:59:07 -0300

    Preloader finalizada
    Implementando as animações de preloader e deixando o SVG animado

---
+++ HTML+CSS+JS/js/main.js
@@ -0,0 +1,19 @@
+//Declarando Variáveis
+var btnContact = document.querySelector('.jl-btn-contact');
+
+//Pagina de preloader
+window.addEventListener('load', function () {
+    var pagePreloder = document.querySelector('.jl-preloader');
+    pagePreloder.classList.add('jl-fade-out');
+ 
+    setTimeout(function () {
+        pagePreloder.style.display = 'none';
+    }, 2000);
+});
+
+//Abrir e fechar contato
+btnContact.addEventListener('click', function(){
+    var boxContact = document.querySelector('.jl-contact-info')
+    boxContact.classList.toggle('jl-is-open');
+    this.classList.toggle('jl-change-icon');
+});
