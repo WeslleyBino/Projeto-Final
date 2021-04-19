567f63caf5556d3357a2f3f2288c56c9689b1d03
Weslley <46580025+WeslleyBino@users.noreply.github.com>
2021-04-19 17:53:12 -0300

    Implementando os botões e o scroll down

--- HTML+CSS+JS/js/main.js
+++ HTML+CSS+JS/js/main.js
@@ -1,5 +1,6 @@
 //Declarando Variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
+var toggleModal = document.querySelectorAll('.jl-toggle-modal');
 
 //Pagina de preloader
 window.addEventListener('load', function () {
@@ -13,7 +14,30 @@ window.addEventListener('load', function () {
 
 //Abrir e fechar contato
 btnContact.addEventListener('click', function(){
-    var boxContact = document.querySelector('.jl-contact-info')
+    var boxContact = document.querySelector('.jl-contact-info');
     boxContact.classList.toggle('jl-is-open');
     this.classList.toggle('jl-change-icon');
 });
+
+//Abrindo e Fechando o Modal de Orcamento
+
+for (var i = 0; i < toggleModal.length; i++){
+    toggleModal[i].addEventListener('click', function(){
+        var overlay = document.querySelector('.jl-overlay');
+        var modalOrcamento = document.querySelector('#jl-modal-orcamento');
+        overlay.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-slide-top-in');
+    });
+}
+
+
+// Animando elementos on Scroll com waypoints
+var myScrollDown = document.querySelector('.jl-scroll-down')
+var waypoint = new Waypoint({
+    element: myScrollDown,
+    handler: function() {
+        myScrollDown.classList.toggle('jl-fade-out')
+    },
+    offset: '80%'
+  })
